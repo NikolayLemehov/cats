53f44e1b6e840482d3ba8a4322ffc15ff979f180
NikolayLemehov <nicklemster@gmail.com>
2022-08-27 15:20:16 +0000

    Deploying to gh-pages from @ NikolayLemehov/cats@4b13d1deeaef048a26b87164585a4d04c877d423 🚀

--- index.a050a5b3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtns:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop--hidden")}e.openModalBtns.forEach((function(e){return e.addEventListener("click",o)})),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.a050a5b3.js.map
